29c196a40c62b7880e0e63cb8b71fc90ccd2984d
Marcos Valério <marcos.valerio.dev@gmail.com>
2021-12-03 09:31:37 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,101 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const users = [];
+const fakeData = {"user":"teste", "room":"teste","teste":"teste", "parametro":"teste", "idFunction":"0"}
+const redis = require('redis');
+const client = redis.createClient( { 
+    url : process.env.REDIS_URL 
+  });
+
+
+
+function saveUserConnection(socket_id, user, room){
+    dataUser={
+        socket_id,
+        user,
+        room
+    }
+    users.push(dataUser);
+}
+
+
+
+setInterval(async ()=>{
+    const values = await client.get('queueFunc');
+    if (values) {
+    const func = JSON.parse(values);
+    //apaga as filas
+    await client.FLUSHALL();
+    io.emit('action', func);
+    }else{
+        io.emit('action', "none");
+    }
+},1000)
+
+    io.on('connect', socket => {
+        socket.on('select_room', (data, callback) => {
+        socket.join(data.room);
+        const userInRoom = users.find((user) => user.user === data.user && user.room === data.room);
+        if (userInRoom)  userInRoom.socket_id = socket.id;
+        else saveUserConnection(socket.id, data.user, data.room);
+         io.emit('signin', socket.id);
+        })
+         
+
+        socket.on('message', async (data) => {
+            //cria cash radis
+            const values = await client.get('queueFunc');
+            if (values) {
+                const func = JSON.parse(values);
+                const data = func.map((item) => {if (item.idFunction != data.idFunction && item.user != data.item)  return item});
+                if (!data) await client.set("queueFunc", JSON.stringify(data));
+            }  
+        })
+
+
+
+        //  setTimeout(()=>{
+        //      console.log("Valor de M:"+m)
+        //     if (m!={})  io.to(m.room).emit('action', m);
+        //  })  
+
+        //     if (callFunction!=[]){
+        //     execCallFunction();
+        //     emitList = getMessagesRoom1();
+        //     io.to(data.room).emit('action', emitList);
+        //     console.log("SALA1: "+listReturn);
+        //     emitList = getMessagesRoom2() 
+        //     io.to(data.room).emit('action', emitList);
+        //     console.log("SALA2: "+listReturn);
+        //     emitList = getMessagesRoom3() 
+        //     io.to(data.room).emit('action', emitList);
+        //     console.log("SALA3: "+listReturn);
+        //     clearCallFunction();
+        // }
+        // },1000)
+    })
+
+function getMessagesRoom1() {
+    const messagesRoom = listReturn.filter(message => message.room ===  sala1);
+    return messagesRoom;
+}
+
+function getMessagesRoom2() {
+    const messagesRoom = listReturn.filter(message => message.room ===  sala2);
+    return messagesRoom;
+}
+
+function getMessagesRoom3() {
+    const messagesRoom = listReturn.filter(message => message.room ===  sala3);
+    return messagesRoom;
+}
+
+server.listen(3000, async () => {
+await client.connect();
+    console.log('Servidor connectado a porta 3000');
+})
